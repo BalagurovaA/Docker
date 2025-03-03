3bad487809d5d118dd2a1d9a5326918037322e13
BalagurovaA <balagurova0473@mail.ru>
2025-03-03 21:43:42 +0300

    всеgit add .! Э

--- src/Part_5_Dockle/serv.c
+++ src/Part_5_Dockle/serv.c
@@ -5,7 +5,19 @@ int main(void)
 {
     while(FCGI_Accept() >= 0)
     {
-        printf("Content-type: text/html\r\nStatus: 200 OK\r\n\r\nHello World!");
+        printf("Content-type: text/html\r\nStatus: 200 oK\r\n\r\n");
+        printf("Hello, World!\n");
+        printf("<pre>\n");
+        printf("-----------------------------\n");
+        printf("| !     !     !     !     ! |\n");
+        printf("|    ?     ?     ?     ?    |\n");
+        printf("|                           |\n");
+        printf("| ?    K R I S T O F S    ? |\n");
+        printf("|                           |\n");
+        printf("|    ?     ?     ?     ?    |\n");
+        printf("| !     !     !     !     ! |\n");
+        printf("-----------------------------\n");
+        printf("<pre>\n");
     }
 
     return 0;
